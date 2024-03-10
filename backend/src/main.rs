5d9eb20b9bbecaedc50de531ab9eb3b55a233ebd
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-03-10 14:46:05 +0100

    feat: basic axum app

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+
+async fn hello() -> String {
+    "Hello, World!".to_string()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let app = axum::Router::new().route("/api/hello", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
