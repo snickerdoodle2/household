47297b87bf5d16715405abf2ccea96ea78a57204
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-04-10 19:35:09 +0200

    feat: added config flags

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 )
 
+type Config struct {
+	port int
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
+}
+
 type App struct {
+	config Config
 	logger *slog.Logger
 }
 
 func main() {
+	var cfg Config
+	flag.IntVar(&cfg.port, "port", 8080, "API Server port")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "Database DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
+	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
