56ea6c7fe501230c58aeb12f556de8bc841fec3d
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-04-14 22:26:55 +0200

    feat: calling get all sensors from endpoint

--- backend/cmd/api/sensors.go
+++ backend/cmd/api/sensors.go
@@ -4,29 +4,16 @@ import (
 	"inzynierka/internal/data"
 	"inzynierka/internal/data/validator"
 	"net/http"
-
-	"github.com/google/uuid"
 )
 
 func (app *App) listSensorsHandler(w http.ResponseWriter, r *http.Request) {
-	sensors := [...]data.Sensor{
-		{
-			ID:          uuid.New(),
-			Name:        "Temperature",
-			URI:         "localhost:9999",
-			Type:        data.DecimalSensor,
-			RefreshRate: 5,
-		},
-		{
-			ID:          uuid.New(),
-			Name:        "Entry doors",
-			URI:         "localhost:11111",
-			Type:        data.BinarySensor,
-			RefreshRate: 10,
-		},
+	sensors, err := app.models.Sensors.GetAll()
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
 	}
 
-	err := app.writeJSON(w, http.StatusOK, envelope{"data": sensors}, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"data": sensors}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
