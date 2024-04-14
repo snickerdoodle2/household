505fa77c3c512f3f96d3e2ac3d5194fe93b2f9b5
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-04-15 00:08:35 +0200

    feat: update movie handler

--- backend/cmd/api/sensors.go
+++ backend/cmd/api/sensors.go
@@ -91,9 +91,67 @@ func (app *App) createSensorHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *App) updateSensorHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: uuid from url + db update
+	sensorIdStr := chi.URLParam(r, "id")
+	sensorId, err := uuid.Parse(sensorIdStr)
 
-	err := app.writeJSON(w, http.StatusNotImplemented, envelope{"status": "todo"}, nil)
+	if err != nil {
+		app.writeJSON(w, http.StatusBadRequest, envelope{"error": "not a valid uuid"}, nil)
+		return
+	}
+
+	sensor, err := app.models.Sensors.Get(sensorId)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	var input struct {
+		Name        *string          `json:"name"`
+		URI         *string          `json:"uri"`
+		Type        *data.SensorType `json:"type"`
+		RefreshRate *int             `json:"refresh_rate"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	if input.Name != nil {
+		sensor.Name = *input.Name
+	}
+
+	if input.URI != nil {
+		sensor.URI = *&sensor.URI
+	}
+
+	if input.Type != nil {
+		sensor.Type = *input.Type
+	}
+
+	if input.RefreshRate != nil {
+		sensor.RefreshRate = *input.RefreshRate
+	}
+
+	v := validator.New()
+	if data.ValidateSensor(v, sensor); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Sensors.Update(sensor)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"sensor": sensor}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
