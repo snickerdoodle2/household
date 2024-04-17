90f1cd4d8af55987625e1f4d98a57348612df36a
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-04-17 15:08:43 +0200

    feat: stopping listener on sensor deletion

--- backend/cmd/api/sensors.go
+++ backend/cmd/api/sensors.go
@@ -209,6 +209,13 @@ func (app *App) deleteSensorHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// stop and remove listener
+	if l, ok := app.listeners[sensorId]; ok {
+		l.GetStopCh() <- struct{}{}
+	}
+
+	delete(app.listeners, sensorId)
+
 	err = app.models.Sensors.Delete(sensorId)
 	if err != nil {
 		switch {
