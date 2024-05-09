9784e16afee89589d1824fe43b207e59624bb087
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-09 10:49:57 +0200

    feat: frontend setup + debug route

---
+++ frontend/src/routes/debug/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    let message = {};
+    let id = "";
+
+    let socket: WebSocket | undefined = undefined;
+
+    const updateSocket = (id) => {
+        if (id.length === 0) return;
+        if (socket) socket.close();
+
+        socket = new WebSocket(`ws://localhost:8080/api/v1/sensor/${id}/value`);
+
+        socket.addEventListener("open", () => {
+            console.log("Opened");
+        });
+
+        socket.addEventListener("message", (data) => {
+            message = JSON.parse(data.data);
+        });
+
+        socket.addEventListener("close", () => {
+            console.log("Closed");
+        });
+    };
+
+    $: {
+        updateSocket(id);
+    }
+</script>
+
+<main>
+    <input bind:value={id} />
+
+    <p>Listening for sensor: <code>{id}</code></p>
+    <code>{JSON.stringify(message)}</code>
+</main>
+
+<style>
+</style>
