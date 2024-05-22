6e402e31b326cd19d915cb6e2a9c770b359adc0f
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-22 13:37:10 +0200

    feat: debug mode

--- frontend/src/routes/debug/+page.svelte
+++ frontend/src/routes/debug/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 import { SERVER_URL } from "$lib/const.js";
-import { type Sensor } from "$lib/types/sensor";
+import * as Select from "$lib/components/ui/select";
+
+import type { PageData } from "./$types";
+
 let message = {};
 let selectedId = "";
 
 const WS_URL = SERVER_URL.replace("http", "ws");
 
-// HACK: generating types not working :(
-export let data: { ids: { data: Sensor[] } };
+export let data: PageData;
 
 let socket: WebSocket | undefined = undefined;
 
@@ -38,11 +40,16 @@ $: {
 </script>
 
 <main class="w-screen h-screen flex flex-col justify-center items-center">
-    <select bind:value={selectedId} placeholder="Select a sensor...">
-        {#each data.ids.data as sensor}
-            <option value={sensor.id}>{sensor.name}</option>
-        {/each}
-    </select>
+    <Select.Root>
+        <Select.Trigger>
+            <Select.Value placeholder="Select a sensor..." />
+        </Select.Trigger>
+        <Select.Content>
+            {#each data.ids.data as sensor}
+                <Select.Item value={sensor.id}>{sensor.name}</Select.Item>
+            {/each}
+        </Select.Content>
+    </Select.Root>
     <p>Listening for sensor: <code>{selectedId}</code></p>
     <code>{JSON.stringify(message)}</code>
 </main>
