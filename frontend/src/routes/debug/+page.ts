82a9df36568d2b2e07f48d8cd83650731f31bcf2
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-22 16:56:43 +0200

    feat: using new helper in debug page

--- frontend/src/routes/debug/+page.ts
+++ frontend/src/routes/debug/+page.ts
@@ -1,24 +1,16 @@
-import { SERVER_URL } from "$lib/const";
-import { Sensor } from "@/types/sensor";
+import { getAllSensors } from "@/helpers/sensor";
 import { error } from "@sveltejs/kit";
-import { z } from "zod";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ fetch }) => {
-    const res = await fetch(`${SERVER_URL}/api/v1/sensor`);
-    const data = await res.json();
-    if (!res.ok) {
-        error(res.status, data);
-    }
+    const sensors = await getAllSensors(fetch);
 
-    const parsed = z.object({ data: Sensor.array() }).safeParse(data);
-    if (!parsed.success) {
-        console.log(parsed.error);
-        error(500, { message: "Error parsing the data" });
+    if (sensors.isError) {
+        error(500, sensors.error);
     }
 
     return {
-        sensors: parsed.data.data.map((e) => {
+        sensors: sensors.data.map((e) => {
             return { label: e.name, value: e.id };
         }),
     };
