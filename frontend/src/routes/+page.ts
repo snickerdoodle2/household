9fb799690a2ef22ddc7a8d48a0a6dd75ab85615a
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-22 17:05:20 +0200

    feat: fetching all sensors from server

---
+++ frontend/src/routes/+page.ts
@@ -0,0 +1,15 @@
+import { getAllSensors } from "@/helpers/sensor";
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({ fetch }) => {
+    const sensors = await getAllSensors(fetch);
+
+    if (sensors.isError) {
+        error(500, sensors.error);
+    }
+
+    return {
+        sensors: sensors.data,
+    };
+};
