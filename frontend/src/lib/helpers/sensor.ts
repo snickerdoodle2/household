7dbe0048db1108464aba94aaa3d87a29ac86cee3
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-22 16:53:08 +0200

    feat: getAllSensors helper

---
+++ frontend/src/lib/helpers/sensor.ts
@@ -0,0 +1,33 @@
+import { SERVER_URL } from "@/const";
+import type { Result } from "@/types/result";
+import { Sensor, type SensorType } from "@/types/sensor";
+import { z } from "zod";
+
+export const getAllSensors = async (
+    fetch: (
+        input: RequestInfo | URL,
+        init?: RequestInit | undefined,
+    ) => Promise<Response>,
+): Promise<Result<SensorType[], string>> => {
+    const res = await fetch(`${SERVER_URL}/api/v1/sensor`);
+    const data = await res.json();
+    if (!res.ok) {
+        return {
+            isError: true,
+            error: data.error,
+        };
+    }
+
+    const parsed = z.object({ data: Sensor.array() }).safeParse(data);
+    if (!parsed.success) {
+        return {
+            isError: true,
+            error: "Error while parsing the data! (getAllSensors)",
+        };
+    }
+
+    return {
+        isError: false,
+        data: parsed.data.data,
+    };
+};
