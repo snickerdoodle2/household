9b9f3d496819972b5ee04e97d170b5923a2ac5ec
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-06-10 14:09:24 +0200

    fix: not found res code

--- backend/cmd/api/errors.go
+++ backend/cmd/api/errors.go
@@ -28,7 +28,7 @@ func (app *App) serverErrorResponse(w http.ResponseWriter, r *http.Request, err 
 
 func (app *App) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
-	app.errorResponse(w, r, http.StatusInternalServerError, message)
+	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 func (app *App) methodNotAllowed(w http.ResponseWriter, r *http.Request) {
