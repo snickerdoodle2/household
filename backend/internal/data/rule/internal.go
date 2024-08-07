1cae30c0ada05f3e9b6abbe6e46e6d567bfcd970
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-08-07 16:43:46 +0200

    feat: or rule

--- backend/internal/data/rule/internal.go
+++ backend/internal/data/rule/internal.go
@@ -52,6 +52,39 @@ func (r *RuleAnd) Dependencies() []uuid.UUID {
 	return res
 }
 
+type RuleOr struct {
+	Children []RuleInternal
+}
+
+func (r *RuleOr) Process(data RuleData) (bool, error) {
+	for _, child := range r.Children {
+		ret, err := child.Process(data)
+		if err != nil {
+			return false, err
+		}
+
+		if ret {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func (r *RuleOr) Dependencies() []uuid.UUID {
+	res := make([]uuid.UUID, len(r.Children))
+
+	for _, child := range r.Children {
+		for _, dep := range child.Dependencies() {
+			if !slices.Contains(res, dep) {
+				res = append(res, dep)
+			}
+		}
+	}
+
+	return res
+}
+
 type RuleGT struct {
 	SensorID uuid.UUID
 	Value    float64
