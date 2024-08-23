16430db07fce206970428a3349e018d4f4e42068
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-08-23 15:00:37 +0200

    feat: Internal rule marshalling

--- backend/internal/data/rule/rule_internal.go
+++ backend/internal/data/rule/rule_internal.go
@@ -1,6 +1,7 @@
 package rule
 
 import (
+	"encoding/json"
 	"errors"
 	"slices"
 
@@ -22,7 +23,19 @@ type RuleInternal interface {
 }
 
 type RuleAnd struct {
-	Children []RuleInternal
+	Children []RuleInternal `json:"children"`
+}
+
+type FakeAnd RuleAnd
+
+func (r RuleAnd) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		FakeAnd
+		Type string `json:"type"`
+	}{
+		FakeAnd: FakeAnd(r),
+		Type:    "and",
+	})
 }
 
 func (r *RuleAnd) Process(data RuleData) (bool, error) {
@@ -55,7 +68,19 @@ func (r *RuleAnd) Dependencies() []uuid.UUID {
 }
 
 type RuleOr struct {
-	Children []RuleInternal
+	Children []RuleInternal `json:"children"`
+}
+
+type FakeOr RuleOr
+
+func (r RuleOr) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		FakeOr
+		Type string `json:"type"`
+	}{
+		FakeOr: FakeOr(r),
+		Type:   "or",
+	})
 }
 
 func (r *RuleOr) Process(data RuleData) (bool, error) {
@@ -88,8 +113,20 @@ func (r *RuleOr) Dependencies() []uuid.UUID {
 }
 
 type RuleGT struct {
-	SensorID uuid.UUID
-	Value    float64
+	SensorID uuid.UUID `json:"sensor_id"`
+	Value    float64   `json:"value"`
+}
+
+type FakeGT RuleGT
+
+func (r RuleGT) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		FakeGT
+		Type string `json:"type"`
+	}{
+		FakeGT: FakeGT(r),
+		Type:   "gt",
+	})
 }
 
 func (r *RuleGT) Process(data RuleData) (bool, error) {
@@ -107,8 +144,20 @@ func (r *RuleGT) Dependencies() []uuid.UUID {
 }
 
 type RuleLT struct {
-	SensorID uuid.UUID
-	Value    float64
+	SensorID uuid.UUID `json:"sensor_id"`
+	Value    float64   `json:"value"`
+}
+
+type FakeLT RuleLT
+
+func (r RuleLT) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		FakeLT
+		Type string `json:"type"`
+	}{
+		FakeLT: FakeLT(r),
+		Type:   "lt",
+	})
 }
 
 func (r *RuleLT) Process(data RuleData) (bool, error) {
