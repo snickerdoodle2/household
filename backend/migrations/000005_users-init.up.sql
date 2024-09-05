7c978fef868a8a56cdea13ae5cae05ff77b5099d
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-05 14:33:12 +0200

    feat: users migration

---
+++ backend/migrations/000005_users-init.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY,
+    username citext UNIQUE NOT NULL,
+    display_name text NOT NULL,
+    password_hash bytea NOT NULL,
+    created_at timestamp(0) NOT NULL DEFAULT NOW(),
+    version integer NOT NULL DEFAULT 1
+);
