9518d22955d75b5a83b1b6cedc7b8a5851d66b77
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-11 15:19:46 +0200

    feat: combining svelte and eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,21 +2,36 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import svelteParser from 'svelte-eslint-parser';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.strict,
     ...tseslint.configs.stylistic,
     {
-        ignores: ['.svelte-kit/*'],
-    },
-    {
         rules: {
             '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
         },
     },
+    ...eslintPluginSvelte.configs['flat/recommended'],
+    eslintConfigPrettier,
+    ...eslintPluginSvelte.configs['flat/prettier'],
     {
-        extends: [eslintConfigPrettier],
+        files: ['*.svelte', '**/*.svelte'],
+        languageOptions: {
+            parser: svelteParser,
+            parserOptions: {
+                parser: tseslint.parser,
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+    },
+    {
+        ignores: ['.svelte-kit/*', 'src/lib/components/ui/*'],
     }
 );
