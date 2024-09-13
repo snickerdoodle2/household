edefa44636c981d5b9a409a7b43d19a0e67a5489
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-13 14:32:08 +0200

    feat: sensor component skeleton

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+    // TODO: calculate grid size
+    import Sensor from '@/components/Sensor.svelte';
     import type { LayoutData } from './$types';
 
     export let data: LayoutData;
 </script>
 
 {#await data.sensors then sensors}
-    {sensors.length}
+    <div class="grid h-full w-full grid-cols-5 grid-rows-6 gap-8 px-32 py-20">
+        {#each sensors as sensor}
+            <Sensor {sensor} />
+        {/each}
+    </div>
 {/await}
