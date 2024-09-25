bddb1df14fe315de17aef33b16dc80c8edaf00c7
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-25 14:52:21 +0200

    feat: create rule page

--- frontend/src/routes/(app)/rules/create/+page.svelte
+++ frontend/src/routes/(app)/rules/create/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+    import * as Card from '$lib/components/ui/card';
+    import { Label } from '$lib/components/ui/label';
+    import * as Select from '$lib/components/ui/select';
+    import FormInput from '@/components/FormInput.svelte';
+    import { Button } from '@/components/ui/button';
     import { onMount } from 'svelte';
-    import type { NewRule } from '$lib/types/rule';
+    import {
+        newRuleSchema,
+        ruleInternalSchema,
+        type NewRule,
+    } from '$lib/types/rule';
     import type { PageData } from './$types';
+    import { authFetch } from '@/helpers/fetch';
 
     export let data: PageData;
 
@@ -36,6 +46,40 @@
         }
     }
 
+    $: if (!loading) {
+        try {
+            const { data, success, error } = ruleInternalSchema.safeParse(
+                JSON.parse(internal)
+            );
+            if (success) {
+                delete errors['payload'];
+                errors = errors;
+                rule.internal = data;
+            } else {
+                console.log(error.issues);
+            }
+        } catch {
+            errors['payload'] = 'Not a valid JSON';
+        }
+    }
+
+    const handleSubmit = async () => {
+        const { success, data, error } = newRuleSchema.safeParse(rule);
+
+        if (!success) {
+            console.log(error.issues);
+            return;
+        }
+
+        const res = await authFetch(
+            '/api/v1/rule',
+            { method: 'POST', body: JSON.stringify(data) },
+            fetch
+        );
+
+        console.log(await res.json());
+    };
+
     onMount(async () => {
         sensors = (await data.sensors).map((e) => ({
             value: e.id,
@@ -47,4 +91,68 @@
 
 {#if loading}
     <p>loading</p>
-{:else}{/if}
+{:else}
+    <Card.Root class="w-[600px] border-none shadow-none">
+        <Card.Header class="text-3xl">
+            <Card.Title>Rule Details</Card.Title>
+        </Card.Header>
+        <Card.Content class="grid grid-cols-[1fr_2fr] items-center gap-3">
+            <FormInput
+                name="name"
+                type="text"
+                label="Name"
+                {errors}
+                bind:value={rule.name}
+            />
+            <FormInput
+                name="description"
+                type="text"
+                label="Description"
+                {errors}
+                bind:value={rule.description}
+            />
+            <Label
+                for="type"
+                class="flex items-center justify-between text-base font-semibold"
+            >
+                To
+                {#if errors['type']}
+                    <span class="text-sm font-normal italic text-red-400"
+                        >{errors['type']}</span
+                    >
+                {/if}
+            </Label>
+            <Select.Root bind:selected={selectedSensor} required name="type">
+                <Select.Trigger
+                    class={errors['type'] ? 'border-2 border-red-600' : ''}
+                >
+                    <Select.Value />
+                </Select.Trigger>
+                <Select.Content>
+                    {#each sensors as type}
+                        <Select.Item value={type.value}
+                            >{type.label}</Select.Item
+                        >
+                    {/each}
+                </Select.Content>
+            </Select.Root>
+            <FormInput
+                name="payload"
+                type="text"
+                label="Payload"
+                {errors}
+                bind:value={payload}
+            />
+            <FormInput
+                name="internal"
+                type="text"
+                label="Internal FIXME"
+                {errors}
+                bind:value={internal}
+            />
+        </Card.Content>
+        <Card.Footer class="flex justify-end gap-3">
+            <Button size="bold" on:click={handleSubmit}>Create</Button>
+        </Card.Footer>
+    </Card.Root>
+{/if}
