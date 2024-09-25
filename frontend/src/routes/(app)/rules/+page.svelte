9b695a2aab370f21fbdbdb06f29d74a2bc2d978a
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-25 12:28:25 +0200

    feat: /rules page

--- frontend/src/routes/(app)/rules/+page.svelte
+++ frontend/src/routes/(app)/rules/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
     import type { PageData } from './$types';
+    import * as Card from '$lib/components/ui/card/index.js';
     export let data: PageData;
 </script>
 
 {#await data.rules then rules}
-    {#each rules as rule}
-        <p>{rule.name}</p>
-    {/each}
+    <div class="flex flex-col gap-2 lg:min-w-[32rem]">
+        {#each rules as rule}
+            <a href={`/rules/${rule.id}`}>
+                <Card.Root class="hover:bg-accent">
+                    <Card.Header>
+                        <Card.Title>
+                            {rule.name}
+                        </Card.Title>
+                        <Card.Description>
+                            {rule.description}
+                        </Card.Description>
+                    </Card.Header>
+                </Card.Root>
+            </a>
+        {/each}
+    </div>
 {/await}
