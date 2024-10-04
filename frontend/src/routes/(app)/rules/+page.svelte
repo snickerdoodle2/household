ca7438f631aee5c9a4af5fc7108486868179d9ed
jakub <jakub@DESKTOP-V8GISMA>
2024-10-04 19:08:40 +0200

    rules page layout

--- frontend/src/routes/(app)/rules/+page.svelte
+++ frontend/src/routes/(app)/rules/+page.svelte
@@ -1,24 +1,43 @@
 <script lang="ts">
     import type { PageData } from './$types';
     import * as Card from '$lib/components/ui/card/index.js';
+    import Button from '@/components/ui/button/button.svelte';
+    import { Plus } from 'radix-icons-svelte';
+    import { Dialog } from 'bits-ui';
+    import DetailsPage from './[id]/+page.svelte';
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+
     export let data: PageData;
 </script>
 
-{#await data.rules then rules}
-    <div class="flex flex-col gap-2 lg:min-w-[32rem]">
+
+<div class="flex h-full items-start">
+    {#await data.rules then rules}
+    <div class="flex-1 grid py-20 gap-8 grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
         {#each rules as rule}
-            <a href={`/rules/${rule.id}`}>
-                <Card.Root class="hover:bg-accent">
-                    <Card.Header>
-                        <Card.Title>
-                            {rule.name}
-                        </Card.Title>
-                        <Card.Description>
-                            {rule.description}
-                        </Card.Description>
-                    </Card.Header>
-                </Card.Root>
-            </a>
+            <div class="lg:min-w-[32rem]">
+                <a href={`/rules/${rule.id}`}>
+                    <Card.Root class="hover:bg-accent">
+                        <Card.Header>
+                            <Card.Title>
+                                {rule.name}
+                            </Card.Title>
+                            <Card.Description>
+                                {rule.description}
+                            </Card.Description>
+                        </Card.Header>
+                    </Card.Root>
+                </a>
+            </div>
         {/each}
+
+        <div class="flex items-center justify-center">
+            <Button variant="outline" size="icon" on:click={() => goto(`/rules/create`)}>
+                <Plus />
+            </Button>
+        </div>
     </div>
-{/await}
+    {/await}
+</div>
+
