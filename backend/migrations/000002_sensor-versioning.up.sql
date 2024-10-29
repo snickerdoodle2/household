6aab459d8cd54de80b454caf5b917ecd4423a067
Krzysztof Dziechciarz <kdziech@student.agh.edu.pl>
2024-10-29 13:25:53 +0100

    fix: timestamp consistency

--- backend/migrations/000002_sensor-versioning.up.sql
+++ backend/migrations/000002_sensor-versioning.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE sensors
-ADD COLUMN created_at timestamp(0) with time zone NOT NULL DEFAULT now(),
+ADD COLUMN created_at timestamptz(0) NOT NULL DEFAULT now(),
 ADD COLUMN version integer NOT NULL DEFAULT 1;
