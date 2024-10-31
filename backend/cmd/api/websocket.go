975b86b69ddc9e3aacf29a4c276f1e0eed5b5e88
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-10-31 13:43:13 +0100

    feat: websocket auth

--- backend/cmd/api/websocket.go
+++ backend/cmd/api/websocket.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"inzynierka/internal/data"
+	"inzynierka/internal/data/validator"
 	"net/http"
 	"sync"
 
@@ -13,7 +17,7 @@ import (
 
 type connStatus struct {
 	mu           sync.Mutex
-	auth         bool
+	authed       bool
 	subscribedTo []uuid.UUID
 }
 
@@ -39,7 +43,7 @@ func (app *App) upgradeSensorWebsocket(w http.ResponseWriter, r *http.Request) {
 	}
 
 	connStatus := &connStatus{
-		auth:         false,
+		authed:       false,
 		subscribedTo: make([]uuid.UUID, 0),
 	}
 
@@ -55,17 +59,77 @@ func (app *App) upgradeSensorWebsocket(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type messageType string
+
+const (
+	authMsg     messageType = "auth"
+	serverError messageType = "server_error"
+)
+
+type websocketMsg struct {
+	Type messageType     `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
+
 func (app *App) handleWebSocketMessage(conn *websocket.Conn, status *connStatus) error {
-	var msg interface{}
+	var msg websocketMsg
+
 	err := wsjson.Read(context.Background(), conn, &msg)
 	if err != nil {
 		return err
 	}
 	app.logger.Debug("new message", "msg", msg)
 
-	if !status.auth {
-		res := map[string]string{"type": "auth", "message": "NO_AUTH"}
-		wsjson.Write(context.Background(), conn, res)
+	if msg.Type == authMsg {
+		return app.handleAuthMsg(conn, status, msg.Data)
+	}
+
+	if !status.authed {
+		res := map[string]string{"type": string(authMsg), "message": "NO_AUTH"}
+		return wsjson.Write(context.Background(), conn, res)
 	}
 	return nil
+}
+
+func (app *App) handleAuthMsg(conn *websocket.Conn, status *connStatus, input json.RawMessage) error {
+	var msgData struct {
+		Token string `json:"token"`
+	}
+
+	err := json.Unmarshal(input, &msgData)
+	if err != nil {
+		return err
+	}
+
+	v := validator.New()
+	if data.ValidateTokenPlaintext(v, msgData.Token); !v.Valid() {
+		return invalidTokenResponse(conn)
+	}
+
+	_, err = app.models.Users.GetForToken(msgData.Token)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			return serverErrorResponse(conn)
+		default:
+			return serverErrorResponse(conn)
+		}
+	}
+
+	status.authed = true
+	return authResponse(conn, "ok")
+}
+
+func invalidTokenResponse(conn *websocket.Conn) error {
+	return authResponse(conn, "INVALID_TOKEN")
+}
+
+func authResponse(conn *websocket.Conn, msg string) error {
+	res := map[string]string{"type": string(authMsg), "message": msg}
+	return wsjson.Write(context.Background(), conn, res)
+}
+
+func serverErrorResponse(conn *websocket.Conn) error {
+	res := map[string]string{"type": string(serverError)}
+	return wsjson.Write(context.Background(), conn, res)
 }
