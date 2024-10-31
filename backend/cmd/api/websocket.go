a513d4fb0d1099dd178be85aeceb652c6454aed0
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-10-31 13:17:55 +0100

    feat: reading messages and responding on no auth

--- backend/cmd/api/websocket.go
+++ backend/cmd/api/websocket.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
+	"github.com/google/uuid"
 )
+
+type connStatus struct {
+	mu           sync.Mutex
+	auth         bool
+	subscribedTo []uuid.UUID
+}
 
 func (app *App) upgradeSensorWebsocket(w http.ResponseWriter, r *http.Request) {
 	const subprotocol = "inzynierka"
@@ -27,4 +37,35 @@ func (app *App) upgradeSensorWebsocket(w http.ResponseWriter, r *http.Request) {
 		conn.Close(websocket.StatusPolicyViolation, fmt.Sprintf("client must speak %s subprotocol", subprotocol))
 		return
 	}
+
+	connStatus := &connStatus{
+		auth:         false,
+		subscribedTo: make([]uuid.UUID, 0),
+	}
+
+	for {
+		err = app.handleWebSocketMessage(conn, connStatus)
+		if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
+			return
+		}
+		if err != nil {
+			app.logger.Error("unhandled ws error", "error", err)
+			return
+		}
+	}
+}
+
+func (app *App) handleWebSocketMessage(conn *websocket.Conn, status *connStatus) error {
+	var msg interface{}
+	err := wsjson.Read(context.Background(), conn, &msg)
+	if err != nil {
+		return err
+	}
+	app.logger.Debug("new message", "msg", msg)
+
+	if !status.auth {
+		res := map[string]string{"type": "auth", "message": "NO_AUTH"}
+		wsjson.Write(context.Background(), conn, res)
+	}
+	return nil
 }
