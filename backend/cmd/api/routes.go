db0c6be0a357c7f999e5a2f9c9d01137b2fb3747
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-10-31 12:18:37 +0100

    feat: websocket stub

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -24,7 +24,7 @@ func (app *App) routes() http.Handler {
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/healthcheck", app.healthcheckHandler)
 
-		// r.Get("/sensor/values", app.getSensorValueHandler)
+		r.Get("/sensor/measurements", app.upgradeSensorWebsocket)
 
 		r.Route("/", func(r chi.Router) {
 			r.Use(app.requireAuthenticated)
---
+++ backend/cmd/api/websocket.go
@@ -0,0 +1,6 @@
+package main
+
+import "net/http"
+
+func (app *App) upgradeSensorWebsocket(w http.ResponseWriter, r *http.Request) {
+}
