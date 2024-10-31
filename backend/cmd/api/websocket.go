dad0de4877c076b339fe4285712eec8ebc44a3bb
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-10-31 13:00:45 +0100

    feat: subprotocol handling

--- backend/cmd/api/websocket.go
+++ backend/cmd/api/websocket.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/coder/websocket"
 )
 
 func (app *App) upgradeSensorWebsocket(w http.ResponseWriter, r *http.Request) {
-	_, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-		Subprotocols: []string{"inzynierka"}, // TODO: better name for subprotocol
+	const subprotocol = "inzynierka"
+	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+		Subprotocols: []string{subprotocol}, // TODO: better name for subprotocol
 	})
 
 	if err != nil {
 		app.logger.Error("open websocket", "error", err)
+		return
+	}
+
+	defer conn.CloseNow()
+
+	app.logger.Debug("new connection", "subprotocol", conn.Subprotocol())
+
+	if conn.Subprotocol() != subprotocol {
+		app.logger.Debug("closing connection", "reason", "wrong subprotocol")
+		conn.Close(websocket.StatusPolicyViolation, fmt.Sprintf("client must speak %s subprotocol", subprotocol))
 		return
 	}
 }
