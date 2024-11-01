edd4ca260b7740bd23e51ecd8455f1e261d2cb20
Krzysztof Dziechciarz <kdziech@student.agh.edu.pl>
2024-11-02 00:40:37 +0100

    feat: load sensor list from config

--- sensors/weather/sensor.py
+++ sensors/weather/sensor.py
@@ -1,9 +1,16 @@
+from processingtype import ProcessingType
+from typing import Any
 from sensortype import SensorType
 
 
 class Sensor:
-    def __init__(self, name: str, refresh_rate: int, uri: str, type: SensorType):
+    def __init__(self, name: str, ip_addr: str, port: int, refresh_rate: int, processing: ProcessingType, number_of_hours: int, params: dict[str, Any]):
         self.name = name
+        self.ip_addr = ip_addr
+        self.port = port
         self.refresh_rate = refresh_rate
-        self.uri = uri
-        self.type = type
+        self.processing = processing
+        self.number_of_hours = number_of_hours
+        self.params = params
+        self.uri = f"{ip_addr}:{port}"
+        self.type = SensorType.DECIMAL_SENSOR
--- sensors/weather/weather-sensor-server.py
+++ sensors/weather/weather-sensor-server.py
@@ -1,8 +1,10 @@
 import json
 import argparse
+from typing import List
 import requests
 from sensor import Sensor
 from sensortype import SensorType
+from processingtype import ProcessingType
 
 parser = argparse.ArgumentParser(
     description="Configure the server credentials")
@@ -11,12 +13,10 @@ parser.add_argument("-u", "--username", type=str,
 parser.add_argument("-p", "--password", type=str,
                     help="Project Server password")
 
-with open("config.json", "r") as file:
-    config = json.load(file)
-
 
 def load_server_config():
-    global srv_ip, srv_port, username, password
+    with open("config.json", "r") as file:
+        config = json.load(file)
 
     args = parser.parse_args()
     username = args.username
@@ -50,7 +50,7 @@ def login(srv_ip, srv_port, username, password) -> str:
         response.raise_for_status()
         return response.json().get("auth_token", {}).get("token")
     except requests.exceptions.RequestException as e:
-        print("Login failed:", e)
+        print("Login failed:", e, response.json().get('error'))
         return None
 
 
@@ -64,7 +64,7 @@ def add_sensor_to_server(srv_ip: str, srv_port: str | int, token: str, sensor: S
     payload = {
         'name': sensor.name,
         'refresh_rate': sensor.refresh_rate,
-        'uri': "127.0.0.1:5005",
+        'uri': sensor.uri,
         'type': sensor.type.value
     }
 
@@ -73,14 +73,46 @@ def add_sensor_to_server(srv_ip: str, srv_port: str | int, token: str, sensor: S
         response.raise_for_status()
         return True
     except requests.exceptions.RequestException as e:
-        print("Adding sensor failed:", e)
+        print("Adding sensor failed:", e, response.json().get('error'))
         return False
+
+
+def load_sensors_from_config() -> List[Sensor]:
+    with open("config.json", "r") as file:
+        config = json.load(file)
+
+    sensors = []
+
+    for sensor_data in config.get("sensors", []):
+        name = sensor_data.get("name")
+        ip_addr = sensor_data.get("ip_addr")
+        port = sensor_data.get("port")
+        refresh_rate = sensor_data.get("refresh_rate")
+
+        processing_str = sensor_data.get("processing")
+        processing = ProcessingType(processing_str) if processing_str else None
+        number_of_hours = sensor_data.get("number_of_hours")
+
+        params = sensor_data.get("params", {})
+
+        sensor = Sensor(
+            name=name,
+            ip_addr=ip_addr,
+            port=port,
+            refresh_rate=refresh_rate,
+            processing=processing,
+            number_of_hours=number_of_hours,
+            params=params
+        )
+
+        sensors.append(sensor)
+
+    return sensors
 
 
 if __name__ == '__main__':
     srv_ip, srv_port, username, password = load_server_config()
     token = login(srv_ip, srv_port, username, password)
-    print(token)
-    test_sensor = Sensor("nazwa", 1200, "127.0.0.1:5555",
-                         SensorType.BINARY_SENSOR)
-    print(add_sensor_to_server(srv_ip, srv_port, token, test_sensor))
+    sensors = load_sensors_from_config()
+    for sensor in sensors:
+        add_sensor_to_server(srv_ip, srv_port, token, sensor)
