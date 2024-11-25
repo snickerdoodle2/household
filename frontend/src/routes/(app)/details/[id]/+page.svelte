7a155b67e7f52b037fa187799d365df60c0fdf55
Jakub <glutek.wosiek@gmail.com>
2024-11-25 13:49:49 +0100

    responsive sensor UI

--- frontend/src/routes/(app)/details/[id]/+page.svelte
+++ frontend/src/routes/(app)/details/[id]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { run } from 'svelte/legacy';
-
     import * as Card from '$lib/components/ui/card';
     import * as Select from '$lib/components/ui/select';
     import NewSensorInput from '$lib/components/FormInput.svelte';
@@ -12,25 +11,31 @@
     import { sensorDetailsSchema, sensorTypeSchema } from '$lib/types/sensor';
     import { z } from 'zod';
     import { authFetch } from '@/helpers/fetch';
+    import * as Dialog from '$lib/components/ui/dialog';
+    import { goto, invalidate } from '$app/navigation';
+    import { SENSOR_URL } from '@/helpers/sensor';
 
     type Props = {
         data: PageData;
-        open: boolean;
     };
 
-    let { data, open = $bindable() }: Props = $props();
+    let { data }: Props = $props();
 
     let editing = $state(false);
     let loading = $state(true);
     let orgSensor: SensorDetails;
-    let sensor: SensorDetails = $state();
-
-    let fieldErrors = $state();
-    run(() => {
-        fieldErrors = {} as Partial<
-            Record<'uri' | 'name' | 'refresh_rate' | 'type', string>
-        >;
+    let sensor: SensorDetails = $state({
+        id: '',
+        name: '',
+        type: 'decimal_sensor',
+        refresh_rate: 0,
+        uri: '',
+        created_at: new Date(),
     });
+
+    let fieldErrors: Partial<
+        Record<'uri' | 'name' | 'refresh_rate' | 'type', string>
+    > = $state({});
     let globalError: string | null = $state(null);
 
     const sensorTypes = sensorTypeSchema.options.map((e) => ({
@@ -39,7 +44,10 @@
         label: e.replace('_', ' '),
     }));
 
-    let selectedType: { value: string; label: string } = $state();
+    let selectedType: { value: string; label: string } = $state({
+        value: '',
+        label: '',
+    });
 
     run(() => {
         if (selectedType) {
@@ -65,7 +73,8 @@
 
         if (res.ok) {
             console.log(await res.json());
-            open = false;
+            await invalidate(SENSOR_URL);
+            close();
         } else {
             const resJson = await res.json();
             console.log(resJson);
@@ -95,7 +104,8 @@
         console.log(resJson);
 
         if (res.ok) {
-            open = false;
+            await invalidate(SENSOR_URL);
+            close();
         } else {
             if (typeof resJson.error === 'string') {
                 globalError = resJson.error;
@@ -114,94 +124,131 @@
         sensor = structuredClone(orgSensor);
         loading = false;
     });
+
+    const close = () => {
+        goto(`/`);
+    };
 </script>
 
-<Card.Root class="w-[512px] border-none shadow-none">
-    {#if loading}
-        <p>Loading...</p>
-    {:else}
-        <Card.Header class="text-3xl">
-            <Card.Title>Sensor Details</Card.Title>
-        </Card.Header>
-        <Card.Content class="grid grid-cols-[3fr_4fr] items-center gap-3">
-            <NewSensorInput
-                name="name"
-                label="Name"
-                bind:value={sensor.name}
-                type="text"
-                errors={fieldErrors}
-            />
-            <NewSensorInput
-                name="refresh_rate"
-                label="Refresh rate"
-                bind:value={sensor.refresh_rate}
-                type="number"
-                errors={fieldErrors}
-            />
-            <NewSensorInput
-                name="uri"
-                label="URI"
-                bind:value={sensor.uri}
-                type="string"
-                errors={fieldErrors}
-            />
-            <Label
-                for="type"
-                class="flex items-center justify-between text-base font-semibold"
-            >
-                Type
-                {#if fieldErrors['type']}
-                    <span class="text-sm font-normal italic text-red-400"
-                        >{fieldErrors['type']}</span
+<Dialog.Root
+    open={true}
+    onOpenChange={(opened) => {
+        if (!opened) {
+            close();
+        }
+    }}
+>
+    <Dialog.Portal>
+        <Dialog.Overlay />
+        <Dialog.Content
+            class="flex max-w-none items-center justify-center px-8 py-4 md:w-fit"
+        >
+            <Card.Root class="w-[512px] border-none shadow-none">
+                {#if loading}
+                    <p>Loading...</p>
+                {:else}
+                    <Card.Header class="text-3xl">
+                        <Card.Title>Sensor Details</Card.Title>
+                    </Card.Header>
+                    <Card.Content
+                        class="grid grid-cols-[3fr_4fr] items-center gap-3"
                     >
-                {/if}
-            </Label>
-            <Select.Root bind:selected={selectedType} required name="type">
-                <Select.Trigger
-                    class={fieldErrors['type'] ? 'border-2 border-red-600' : ''}
-                >
-                    <Select.Value />
-                </Select.Trigger>
-                <Select.Content>
-                    {#each sensorTypes as type}
-                        <Select.Item value={type.value}
-                            >{type.label}</Select.Item
+                        <NewSensorInput
+                            name="name"
+                            label="Name"
+                            bind:value={sensor.name}
+                            type="text"
+                            errors={fieldErrors}
+                            disabled={!editing}
+                        />
+                        <NewSensorInput
+                            name="refresh_rate"
+                            label="Refresh rate"
+                            bind:value={sensor.refresh_rate}
+                            type="number"
+                            errors={fieldErrors}
+                            disabled={!editing}
+                        />
+                        <NewSensorInput
+                            name="uri"
+                            label="URI"
+                            bind:value={sensor.uri}
+                            type="string"
+                            errors={fieldErrors}
+                            disabled={!editing}
+                        />
+                        <Label
+                            for="type"
+                            class="flex items-center justify-between text-base font-semibold"
                         >
-                    {/each}
-                </Select.Content>
-            </Select.Root>
-        </Card.Content>
-        <Card.Footer class="flex justify-end gap-3">
-            <div class="flex w-full flex-col items-center justify-center gap-4">
-                {#if globalError}
-                    <p class="mt-1 text-sm text-red-500">{globalError}</p>
-                {/if}
+                            Type
+                            {#if fieldErrors['type']}
+                                <span
+                                    class="text-sm font-normal italic text-red-400"
+                                    >{fieldErrors['type']}</span
+                                >
+                            {/if}
+                        </Label>
+                        <Select.Root
+                            bind:selected={selectedType}
+                            required
+                            name="type"
+                            disabled={!editing}
+                        >
+                            <Select.Trigger
+                                class={fieldErrors['type']
+                                    ? 'border-2 border-red-600'
+                                    : ''}
+                            >
+                                <Select.Value />
+                            </Select.Trigger>
+                            <Select.Content>
+                                {#each sensorTypes as type}
+                                    <Select.Item value={type.value}
+                                        >{type.label}</Select.Item
+                                    >
+                                {/each}
+                            </Select.Content>
+                        </Select.Root>
+                    </Card.Content>
+                    <Card.Footer class="flex justify-end gap-3">
+                        <div
+                            class="flex w-full flex-col items-center justify-center gap-4"
+                        >
+                            {#if globalError}
+                                <p class="mt-1 text-sm text-red-500">
+                                    {globalError}
+                                </p>
+                            {/if}
 
-                <div class="flex w-full justify-end gap-3">
-                    {#if editing}
-                        <Button
-                            variant="destructive"
-                            size="bold"
-                            on:click={handleDelete}>Delete</Button
-                        >
-                        <Button
-                            variant="outline"
-                            size="bold"
-                            on:click={handleCancel}>Cancel</Button
-                        >
-                        <Button size="bold" on:click={handleSubmit}
-                            >Submit</Button
-                        >
-                    {:else}
-                        <Button
-                            on:click={() => {
-                                editing = true;
-                            }}
-                            size="bold">Edit</Button
-                        >
-                    {/if}
-                </div>
-            </div>
-        </Card.Footer>
-    {/if}
-</Card.Root>
+                            <div class="flex w-full justify-end gap-3">
+                                {#if editing}
+                                    <Button
+                                        variant="destructive"
+                                        size="bold"
+                                        on:click={handleDelete}>Delete</Button
+                                    >
+                                    <Button
+                                        variant="outline"
+                                        size="bold"
+                                        on:click={handleCancel}>Cancel</Button
+                                    >
+                                    <Button size="bold" on:click={handleSubmit}
+                                        >Submit</Button
+                                    >
+                                {:else}
+                                    <Button
+                                        on:click={() => {
+                                            editing = true;
+                                        }}
+                                        size="bold">Edit</Button
+                                    >
+                                {/if}
+                            </div>
+                        </div>
+                    </Card.Footer>
+                {/if}
+            </Card.Root>
+        </Dialog.Content>
+    </Dialog.Portal>
+</Dialog.Root>
