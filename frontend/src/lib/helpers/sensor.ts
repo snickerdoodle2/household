c080657fcb6cb4e92b63f4a2eccf2983f94cce11
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-11-25 12:17:13 +0100

    fix: undo lib/helpers/sensor.ts changes

--- frontend/src/lib/helpers/sensor.ts
+++ frontend/src/lib/helpers/sensor.ts
@@ -1,14 +1,18 @@
 import type { Result } from '@/types/result';
+import {
+    sensorDetailsSchema,
+    sensorSchema,
+    type Sensor,
+    type SensorDetails,
+} from '@/types/sensor';
 import { z } from 'zod';
 import { authFetch } from './fetch';
 import type { FetchFn } from '@/types/misc';
-import { userSchema, type User } from '@/types/user';
-import { sensorDetailsSchema, type SensorDetails } from '@/types/sensor';
 
 export const getAllSensors = async (
     fetch: FetchFn
-): Promise<Result<User[], string>> => {
-    const res = await authFetch(`/api/v1/sensor/`, {}, fetch);
+): Promise<Result<Sensor[], string>> => {
+    const res = await authFetch(`/api/v1/sensor`, {}, fetch);
     const data = await res.json();
     if (!res.ok) {
         return {
@@ -17,11 +21,11 @@ export const getAllSensors = async (
         };
     }
 
-    const parsed = z.object({ data: userSchema.array() }).safeParse(data);
+    const parsed = z.object({ data: sensorSchema.array() }).safeParse(data);
     if (!parsed.success) {
         return {
             isError: true,
-            error: 'Error while parsing the data! (getAllUsers)',
+            error: 'Error while parsing the data! (getAllSensors)',
         };
     }
 
