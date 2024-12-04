f612c6941f2c259ea6aeaa8495c19fa219875b6e
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-04 16:31:44 +0100

    feat: more helpers

--- backend/internal/data/notifications.go
+++ backend/internal/data/notifications.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -23,6 +24,11 @@ type Notification struct {
 	Title       string
 	Description string
 	CreatedAt   time.Time
+}
+
+type UserNotification struct {
+	Notification
+	Read bool
 }
 
 type NotificationModel struct {
@@ -49,4 +55,59 @@ func (m *NotificationModel) Insert(notification *Notification) error {
 	defer cancel()
 
 	return m.DB.QueryRow(ctx, query, args...).Scan(notification.CreatedAt)
+}
+
+func (m *NotificationModel) InsertForUsers(notification *Notification, users []uuid.UUID) error {
+	err := m.Insert(notification)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = m.DB.CopyFrom(ctx,
+		pgx.Identifier{"user_notifications"},
+		[]string{"notification_id", "user_id"},
+		pgx.CopyFromSlice(len(users), func(i int) ([]any, error) {
+			return []any{notification.ID, users[i]}, nil
+		}),
+	)
+
+	return err
+}
+
+func (m *NotificationModel) InsertUser(notificationId, userId uuid.UUID) error {
+	query := `
+    INSERT INTO user_notifications (notification_id, user_id)
+    VALUES ($1, $2)
+    `
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := m.DB.Exec(ctx, query, notificationId, userId)
+
+	// TODO: Handle user already notified
+	return err
+}
+
+func (m *NotificationModel) MarkAsRead(notificationId, userId uuid.UUID) error {
+	query := `
+    UPDATE user_notifications
+    SET read = true
+    WHERE notification_id = $1 AND user_id = $2
+    `
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := m.DB.Exec(ctx, query, notificationId, userId)
+
+	// TODO: Handle user already read this
+	return err
+}
+
+func (m *NotificationModel) GetUnread(userId uuid.UUID) error {
+	return nil
 }
