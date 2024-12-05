bf0e850a689303f328a26b5c3be56dee1411e41a
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-05 15:02:37 +0100

    feat: added tooltip

--- frontend/src/lib/components/notifications/Notification.svelte
+++ frontend/src/lib/components/notifications/Notification.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import * as Alert from '$lib/components/ui/alert';
+    import * as Tooltip from '$lib/components/ui/tooltip';
     import type {
         NotificationLevel,
         Notification,
@@ -16,21 +17,30 @@
 
 {#snippet icon(level: NotificationLevel)}
     {@const common = 'w-7 h-7'}
-    {#if level == 'info'}
-        <BadgeInfo class={cn(common, 'stroke-sky-500')} />
-    {:else if level == 'success'}
-        <BadgeCheck class={cn(common, 'stroke-emerald-500')} />
-    {:else if level == 'error'}
-        <BadgeX class={cn(common, 'stroke-red-600')} />
-    {:else if level == 'warning'}
-        <BadgeAlert class={cn(common, 'stroke-amber-300')} />
-    {/if}
+    <Tooltip.Root>
+        <Tooltip.Trigger>
+            {#if level == 'info'}
+                <BadgeInfo class={cn(common, 'stroke-sky-500')} />
+            {:else if level == 'success'}
+                <BadgeCheck class={cn(common, 'stroke-emerald-500')} />
+            {:else if level == 'error'}
+                <BadgeX class={cn(common, 'stroke-red-600')} />
+            {:else if level == 'warning'}
+                <BadgeAlert class={cn(common, 'stroke-amber-300')} />
+            {/if}
+        </Tooltip.Trigger>
+        <Tooltip.Content>{level}</Tooltip.Content>
+    </Tooltip.Root>
 {/snippet}
 
 <Alert.Root class="flex gap-4 items-center">
     {@render icon(notification.level)}
     <div class="">
         <Alert.Title>{notification.title}</Alert.Title>
-        <Alert.Description>{notification.description}</Alert.Description>
+        <Alert.Description>
+            <p>
+                {notification.description}
+            </p>
+        </Alert.Description>
     </div>
 </Alert.Root>
