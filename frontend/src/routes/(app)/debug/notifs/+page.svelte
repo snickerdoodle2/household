6a720970f1f88848b3f625e9c50a0df4d4e79ea4
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-05 14:03:42 +0100

    feat: notification debug page

---
+++ frontend/src/routes/(app)/debug/notifs/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { AppWebsocket } from '@/helpers/socket.svelte';
+
+    const ws = new AppWebsocket();
+</script>
+
+<ol>
+    {#each ws.notifications as notification}
+        <li>
+            ({notification.level}) {notification.title} - {notification.description}
+        </li>
+    {/each}
+</ol>
