28455420b0ec9d6baa3ea44397c7d4e687b01747
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-10 14:45:59 +0100

    feat: users table

--- frontend/src/routes/(app)/settings/users/+page.svelte
+++ frontend/src/routes/(app)/settings/users/+page.svelte
@@ -12,29 +12,35 @@
     const { users } = data;
 </script>
 
-<Table.Root class="max-w-[1024px] mx-auto">
-    <Table.Header>
-        <Table.Row>
-            <Table.Cell>Username</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Created At</Table.Cell>
-            <Table.Cell>Action</Table.Cell>
-        </Table.Row>
-    </Table.Header>
-    <Table.Body>
-        {#each users as user}
+<div class="max-w-[1024px] mx-auto flex flex-col items-end">
+    <Table.Root class="mb-8">
+        <Table.Header>
             <Table.Row>
-                <Table.Cell>{user.username}</Table.Cell>
-                <Table.Cell>{user.name}</Table.Cell>
-                <Table.Cell>{user.created_at.toLocaleString()}</Table.Cell>
-                <Table.Cell
-                    ><Button
-                        size="icon"
-                        variant="ghost"
-                        href={`/settings/users/${user.id}`}><UserPen /></Button
-                    ></Table.Cell
-                >
+                <Table.Cell>Username</Table.Cell>
+                <Table.Cell>Name</Table.Cell>
+                <Table.Cell>Created At</Table.Cell>
+                <Table.Cell>Action</Table.Cell>
             </Table.Row>
-        {/each}
-    </Table.Body>
-</Table.Root>
+        </Table.Header>
+        <Table.Body>
+            {#each users as user}
+                <Table.Row>
+                    <Table.Cell>{user.username}</Table.Cell>
+                    <Table.Cell>{user.name}</Table.Cell>
+                    <Table.Cell>{user.created_at.toLocaleString()}</Table.Cell>
+                    <Table.Cell
+                        ><Button
+                            size="icon"
+                            variant="ghost"
+                            href={`/settings/users/${user.id}`}
+                            ><UserPen /></Button
+                        ></Table.Cell
+                    >
+                </Table.Row>
+            {/each}
+        </Table.Body>
+    </Table.Root>
+
+    <!-- TODO: CREATE USER PAGE -->
+    <Button>Create</Button>
+</div>
---
+++ frontend/src/routes/(app)/settings/users/[id]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<p>Details of {$page.url.pathname.split('/')[3]}</p>
