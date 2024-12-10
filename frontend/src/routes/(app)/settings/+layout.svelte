b9cf308a2ad11d06f341f4391bc9105f3a406b32
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-10 13:59:17 +0100

    feat: settings page

---
+++ frontend/src/routes/(app)/settings/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { Button } from '@/components/ui/button';
+    import type { Snippet } from 'svelte';
+
+    const LINKS = [
+        {
+            label: 'Users',
+            href: '/settings/users',
+        },
+    ] as const;
+
+    type Props = {
+        children: Snippet;
+    };
+
+    const { children }: Props = $props();
+</script>
+
+{#snippet Link(label: string, href: string)}
+    <Button
+        variant="link"
+        size={$page.url.pathname === href ? 'bold' : 'default'}
+        class="text-base"
+        disabled={$page.url.pathname === href}
+        {href}
+    >
+        {label}
+    </Button>
+{/snippet}
+
+<div class="w-full h-full flex flex-row mt-32 ml-40 gap-8">
+    <nav>
+        {#each LINKS as link (link.href)}
+            {@render Link(link.label, link.href)}
+        {/each}
+    </nav>
+    <main class="flex-1">{@render children()}</main>
+</div>
