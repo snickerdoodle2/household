827e4b61f8671425d798ddc33d18cd884b2674c7
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-10 14:25:36 +0100

    feat: loading data from backend

--- frontend/src/routes/(app)/settings/users/+page.svelte
+++ frontend/src/routes/(app)/settings/users/+page.svelte
@@ -1,1 +1,12 @@
-<p>users</p>
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    type Props = {
+        data: PageData;
+    };
+
+    const { data }: Props = $props();
+    const { users } = data;
+</script>
+
+{JSON.stringify(users)}
