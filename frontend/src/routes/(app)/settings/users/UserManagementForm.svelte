bf92fca19f80b1e09775ad2bfbfcc75c3f88bb2e
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-11 13:18:00 +0100

    feat: moved UserManagmentForm

---
+++ frontend/src/routes/(app)/settings/users/UserManagementForm.svelte
@@ -0,0 +1,143 @@
+<script lang="ts">
+    import { userSchema, type User } from '@/types/user';
+    import NewSensorInput from '$lib/components/FormInput.svelte';
+    import Button from '$lib/components/ui/button/button.svelte';
+    import { authFetch } from '@/helpers/fetch';
+
+    type Props =
+        | {
+              user: User;
+              action: 'edit';
+          }
+        | {
+              user: undefined;
+              action: 'add';
+          };
+
+    const props: Props = $props();
+
+    let user = $state(
+        props.user ?? {
+            username: '',
+            name: '',
+        }
+    );
+
+    const cancelEditing = () => {
+        user = props.user ?? {
+            username: '',
+            name: '',
+        };
+        editing = false;
+    };
+
+    let editing = $state(false);
+
+    let globalError: string | undefined = $state();
+    let fieldErrors: Record<string, string> = $state({});
+
+    async function handleSubmit() {
+        const { data, success, error } = userSchema.safeParse({
+            username: user.username,
+            name: user.name,
+        });
+
+        if (!success) {
+            console.log(error.issues);
+            fieldErrors = {};
+            for (const issue of error.issues) {
+                fieldErrors[issue.path[0]] = issue.message;
+            }
+            return;
+        }
+
+        const res = await authFetch(`/api/v1/user/${data.username}`, {
+            method: props.action === 'add' ? 'POST' : 'PUT',
+            body: JSON.stringify(data),
+        });
+
+        const resJson = await res.json();
+        console.log(resJson);
+
+        if (typeof resJson.error === 'string') {
+            globalError = resJson.error;
+        } else {
+            fieldErrors = resJson.error;
+        }
+    }
+</script>
+
+<div>
+    <h3 class="text-3xl">
+        {props.action === 'add' ? 'Add User' : 'User Details'}
+    </h3>
+    <div class="grid grid-cols-2 gap-2 p-4 pb-2">
+        <NewSensorInput
+            name="name"
+            label="Name"
+            bind:value={user.name}
+            disabled={!editing}
+            type="text"
+            errors={fieldErrors}
+        />
+        <NewSensorInput
+            name="username"
+            label="Username"
+            bind:value={user.username}
+            disabled={!editing}
+            type="text"
+            errors={fieldErrors}
+        />
+        <!-- <Label -->
+        <!--     for="type" -->
+        <!--     class="flex items-center justify-between text-base font-semibold" -->
+        <!-- > -->
+        <!--     Role -->
+        <!--     {#if fieldErrors['type']} -->
+        <!--         <span class="text-sm font-normal italic text-red-400" -->
+        <!--             >{fieldErrors['type']}</span -->
+        <!--         > -->
+        <!--     {/if} -->
+        <!-- </Label> -->
+        <!-- <Select.Root bind:selected={selectedRole} required name="type"> -->
+        <!--     <Select.Trigger -->
+        <!--         class={fieldErrors['type'] ? 'border-2 border-red-600' : ''} -->
+        <!--     > -->
+        <!--         <Select.Value /> -->
+        <!--     </Select.Trigger> -->
+        <!--     <Select.Content> -->
+        <!--         <Select.Item value={'user'}>User</Select.Item> -->
+        <!--         <Select.Item value={'admin'}>Admin</Select.Item> -->
+        <!--     </Select.Content> -->
+        <!-- </Select.Root> -->
+    </div>
+
+    {#if globalError}
+        <p class="mb-1 text-center text-sm text-red-500">{globalError}</p>
+    {/if}
+    {#if props.action === 'edit'}
+        <div class="flex justify-end p-2 gap-4">
+            <!-- TODO: DISABLE IF THIS USER IS CURRENT USER -->
+            <Button
+                size="bold"
+                on:click={handleSubmit}
+                disabled={!editing}
+                variant="destructive">Delete</Button
+            >
+            {#if editing}
+                <Button size="bold" on:click={cancelEditing} variant="outline"
+                    >Cancel</Button
+                >
+            {:else}
+                <Button
+                    variant="outline"
+                    size="bold"
+                    on:click={() => {
+                        editing = true;
+                    }}>Edit</Button
+                >
+            {/if}
+            <Button size="bold" on:click={handleSubmit}>Submit</Button>
+        </div>
+    {:else}{/if}
+</div>
